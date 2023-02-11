9e0797c0ecf73cfb2d1b44be78473aef8cb50412
kristjan-kure <kristjan.kure.1@gmail.com>
2023-02-11 13:54:52 +0200

    add week16 code

---
+++ Week16JSON/BitCoinRate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16JSON
+{
+    public class BitCoinRate
+    {
+        public bpi bpi
+        {
+            get; set;
+        }
+    }
+
+    public class bpi
+    {
+        public EUR EUR { get; set;}
+    }
+
+    public class EUR
+    {
+        public string code
+        {
+            get;set;
+        }
+        public float rate_float
+        {
+            get; set;
+        }
+    }
+}
